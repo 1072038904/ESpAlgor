035bfd5f5a1c226ae2d96a600f7b8236b1a1fd06
沈维郑 <1103107216@qq.com>
2018-05-05 19:27:09 +0800

    增加字段说明： 范围名称

--- src/main/java/com/dao/RangeThresholdDao.java
+++ src/main/java/com/dao/RangeThresholdDao.java
@@ -2,7 +2,11 @@ package com.dao;
 
 import com.pojo.RangeThreshold;
 
+import java.util.List;
+
 public interface RangeThresholdDao {
     //判断该实体是否在在数据库中
-    public int isHotExisted(RangeThreshold rangeThreshold);
+    public RangeThreshold isHotExisted(RangeThreshold rangeThreshold);
+    // 根据rangeName查询所有的点
+    public List<RangeThreshold> allPointByRangeName(String rangeName);
 }
