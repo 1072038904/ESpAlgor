5876acf9ff607510b8b70bf9c296f1897e552f4d
haonanxiaodi <869328909@qq.com>
2018-05-05 20:12:32 +0800

    11

--- src/main/java/com/dao/ClubInfroDao.java
+++ src/main/java/com/dao/ClubInfroDao.java
@@ -5,7 +5,7 @@ import com.pojo.Club;
 public interface ClubInfroDao {
     // 根据俱乐部的id查询俱乐部所有信息
     public Club getClubInfroById(Integer clubId);
-    // 添加新的俱乐部
+    // 添加新的俱乐部3
     public int addNewClub(Club club);
     // 根据id删除俱乐部
     public int deleteClub(Integer clubId);
